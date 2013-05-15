1438bd4d34d250ebfcffd6fb72418a1ccf3aa187
Denis Nelubin <dnelubin@gmail.com>
2013-05-15 23:56:54 +0700

    Created project.

---
+++ src/ru/gelin/android/getter/ButtonActivity.java
@@ -0,0 +1,15 @@
+package ru.gelin.android.getter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ButtonActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
