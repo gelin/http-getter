f65606ff2884395a8cefe7e795b573a6e0863c4b
Denis Nelubin <dnelubin@gmail.com>
2013-05-19 14:30:55 +0700

    Added progress indicator.

--- src/ru/gelin/android/getter/GetActivity.java
+++ src/ru/gelin/android/getter/GetActivity.java
@@ -3,17 +3,22 @@ package ru.gelin.android.getter;
 import android.app.Activity;
 import android.net.http.SslError;
 import android.os.Bundle;
+import android.view.Window;
 import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class GetActivity extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.get);
         WebView web = (WebView)findViewById(R.id.web);
+        web.setWebChromeClient(new MyWebChromeClient());
         web.setWebViewClient(new MyWebViewClient());
+        setProgressBarVisibility(true);
         web.loadUrl(getIntent().getDataString());
     }
 
@@ -35,6 +40,14 @@ public class GetActivity extends Activity {
             }
         }
 
+    }
+
+    class MyWebChromeClient extends WebChromeClient {
+        public void onProgressChanged(WebView view, int progress) {
+            // Activities and WebViews measure progress with different scales.
+            // The progress meter will automatically disappear when we reach 100%
+            setProgress(progress * 100);
+        }
     }
 
 }
